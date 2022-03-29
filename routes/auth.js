4bcf80f43373b82f8b3921479db345319be148ae
lokendra <lokendrachaulagain803@gmail.com>
2022-03-29 07:29:20 +0545

    user login authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');//its async function
+const bcrypt = require('bcrypt');//its async function so we need to use await
 
 
-//REGISTER ROUTE
+//REGISTER USER
 router.post("/register", async (req, res) => {
     try {
         //generate salt to hash the password
@@ -25,8 +25,38 @@ router.post("/register", async (req, res) => {
 
         //if error show error
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ error });
 
+    }
+})
+
+
+//LOGIN USER
+router.post("/login", async (req, res) => {
+    try {
+        //find the user by email
+        const user = await User.findOne({ email: req.body.email });
+
+        //if user not found return error
+        if (!user) {
+            return res.status(400).json({ msg: "User not found" });
+        }
+
+        //If user found compare the user given password with hashed password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+
+        //if password is not match return error
+        if (!validPassword) {
+            return res.status(400).json({ msg: "Password is not match" });
+        }
+
+        //if user is found and password is match return user
+        res.status(200).json(user);
+
+        //if error catch it and show error
+    } catch (error) {
+        res.status(500).json(error);
     }
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-//create Router
 const router = require('express').Router();
 
 
