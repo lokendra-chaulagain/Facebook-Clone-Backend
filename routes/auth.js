f799343c617a40ba64d8a9b11c1b84cb7e0846a5
lokendra <lokendrachaulagain803@gmail.com>
2022-04-03 12:52:15 +0545

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,6 @@ router.post("/register", async (req, res) => {
 })
 
 
-
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
@@ -58,68 +57,3 @@ router.post("/login", async (req, res) => {
 })
 
 module.exports = router
-
-
-
-
-
-//----------------------------------------------
-// //REGISTER A USER
-// router.post("/register", async (req, res) => {
-//   try {
-//     //generate salt to hash password
-//     const  salt = await bcrypt.genSalt(10)
-
-//     //hash the password
-//     const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-//     //register a new user
-//     const newUser = await new User({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: hashedPassword
-//     })
-
-//     //save the user
-//     const user = await newUser.save()
-//     res.status(200).json(user)
-
-
-
-//   } catch (error) {
-//     res.status(500).json({ message: error.message })
-
-//   }
-// })
-
-
-
-
-
-// //LOGIN USER
-// router.post('/login', async (req, res) => {
-//   try {
-//     //find the user by email
-//     const user = await User.findOne({ email: req.body.email })
-
-//     //if user not found return error
-//     if (!user) {
-//       return res.status(400).json({ msg: 'User not found' })
-//     }
-
-//     //If user found compare the user given password with hashed password
-//     const validPassword = await bcrypt.compare(req.body.password, user.password)
-
-//     //if password is not match return error
-//     if (!validPassword) {
-//       return res.status(400).json({ msg: 'Password is not match' })
-//     }
-
-//     //if user is found and password is match return user
-//     res.status(200).json(user)
-
-//     //if error catch it and show error
-//   } catch (error) {
-//     res.status(500).json(error)
-//   }
-// })
