3e2dd3f2e9c205a9877b38c3fcf5d63930f830ac
lokendra <lokendrachaulagain803@gmail.com>
2022-04-03 06:06:09 +0545

    post update

--- routes/posts.js
+++ routes/posts.js
@@ -15,25 +15,42 @@ router.post("/", async (req, res) => {
     }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //UPDATE A POST
+router.put("/:id", async (req, res) => { //post id
+
+    try {
+
+        const post = await Post.findById(req.params.id) //find the post by id
+        //check the owner of the post
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body })
+            res.status(200).json({ message: "Post updated" })
+
+
+        }
+        else {
+            res.status(403).json({ message: "You are not authorized to update this post" })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 //DELETE A POST
