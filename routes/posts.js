d9e8fd090d79f71fae150a0d4ee2dbe077e7ce3f
lokendra <lokendrachaulagain803@gmail.com>
2022-04-03 06:13:29 +0545

    post delete

--- routes/posts.js
+++ routes/posts.js
@@ -25,8 +25,6 @@ router.put("/:id", async (req, res) => { //post id
         if (post.userId === req.body.userId) {
             await post.updateOne({ $set: req.body })
             res.status(200).json({ message: "Post updated" })
-
-
         }
         else {
             res.status(403).json({ message: "You are not authorized to update this post" })
@@ -36,9 +34,28 @@ router.put("/:id", async (req, res) => { //post id
         res.status(500).json({ message: error.message })
 
     }
+})
 
 
+//DELETE A POST
+router.delete("/:id", async (req, res) => { //post id
 
+    try {
+
+        const post = await Post.findById(req.params.id) //find the post by id
+        //check the owner of the post
+        if (post.userId === req.body.userId) {
+            await post.deleteOne()
+            res.status(200).json({ message: "Post has neen delted" })
+        }
+        else {
+            res.status(403).json({ message: "You are not authorized to delete this post" })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
 })
 
 
@@ -52,8 +69,6 @@ router.put("/:id", async (req, res) => { //post id
 
 
 
-
-//DELETE A POST
 
 //LIKE A POST 
 
